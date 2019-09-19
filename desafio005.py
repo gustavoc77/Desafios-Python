3ed4bab582978ee89715d37a840123ecb45c9cf0
Gustavo <gustavocosta060@gmail.com>
2019-09-18 21:10:03 -0300

    Desafio 5 - Curso em Vídeo

---
+++ desafio005.py
@@ -0,0 +1,3 @@
+# faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
+n1 = int(input('Digite um número:'))
+print('Número {} \nAntecessor  = {} \nSucessor = {}'.format(n1,n1-1,n1+1))

fd7b178aa6501e6d635371d8da7d968be6ebe3d6
Gustavo <gustavocosta060@gmail.com>
2019-09-18 21:04:01 -0300

    Desafio 2 - Curso em Vídeo

---
+++ desafio2.py
@@ -0,0 +1,5 @@
+nome = input('Qual é o seu nome?')
+dia = input ('Dia do nascimento:')
+mes = input('mês do nascimento:')
+ano = input ('Ano do nascimento:')
+print(nome,' você nasceu no ',dia,' de ',mes,' de ',ano,'.' , 'Correto?')
